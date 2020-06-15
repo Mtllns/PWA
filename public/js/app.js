2852d710a3e8661efd9e4d6fa3c0ef42b9004dd2
Mtllns <miquelmtllns@pm.me>
2020-06-15 12:21:07 +0200

    POST con coordenandas y mapa

--- public/js/app.js
+++ public/js/app.js
@@ -80,7 +80,7 @@ var usuario;
 
 // ===== Codigo de la aplicación
 
-function crearMensajeHTML(mensaje, personaje, lat, lng, foto) {
+function crearMensajeHTML(mensaje, personaje, lat, lng) {
 
     // console.log(mensaje, personaje, lat, lng);
 
@@ -99,12 +99,12 @@ function crearMensajeHTML(mensaje, personaje, lat, lng, foto) {
                 ${ mensaje }
                 `;
     
-    if ( foto ) {
-        content += `
-                <br>
-                <img class="foto-mensaje" src="${ foto }">
-        `;
-    }
+    // if ( foto ) {
+    //     content += `
+    //             <br>
+    //             <img class="foto-mensaje" src="${ foto }">
+    //     `;
+    // }
         
     content += `</div>        
                 <div class="arrow"></div>
@@ -244,8 +244,7 @@ postBtn.on('click', function() {
         mensaje: mensaje,
         user: usuario,
         lat: lat,
-        lng: lng,
-        foto: foto
+        lng: lng
     };
 
 
@@ -260,10 +259,8 @@ postBtn.on('click', function() {
     .then( res => console.log( 'app.js', res ))
     .catch( err => console.log( 'app.js error:', err ));
 
-    camera.apagar();
-    contenedorCamara.addClass('oculto');
-
-    crearMensajeHTML( mensaje, usuario, lat, lng, foto );
+    
+    crearMensajeHTML( mensaje, usuario, lat, lng );
     
     foto = null;
 });
@@ -499,6 +496,9 @@ btnLocation.on('click', () => {
     navigator.geolocation.getCurrentPosition( pos => {
         console.log( pos );
         mostrarMapaModal( pos.coords.latitude, pos.coords.longitude );
+
+        lat =  pos.coords.latitude;
+        lng = pos.coords.longitude;
 
 
     });
